2e629a6503a8bd9e135a3a630d137c7ae4f465b5
Ishmael Martin <imartin@outlook.com>
2020-01-08 14:42:00 -0500

    Edited settings file and added a .env file.

--- Backend/checkbook/settings.py
+++ Backend/checkbook/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qb6&g4u+#!ctv8bap2o)67=-$770mc5^te%b%ieph@*7rc3q+n'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
